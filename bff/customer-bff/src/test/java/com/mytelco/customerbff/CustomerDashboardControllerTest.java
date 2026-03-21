21601672c54eda67c282f3f4a5661a112c652aa2
Richard Hendricks <richard@tech-radar.local>
2026-03-21 17:06:07 +0000

    fix(test): repair customer-bff analytics test compilation

--- bff/customer-bff/src/test/java/com/mytelco/customerbff/CustomerDashboardControllerTest.java
+++ bff/customer-bff/src/test/java/com/mytelco/customerbff/CustomerDashboardControllerTest.java
@@ -37,6 +37,7 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
